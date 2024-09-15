bdef1580837d566bd9610599b62c76be44fc3bd9
Petro Yatskiv <Petro_Yatskiv@epam.com>
2024-09-15 20:34:37 +0300

    feat: add memory caching

--- bff/src/controllers/productDetailsController.ts
+++ bff/src/controllers/productDetailsController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-import { getProductDetails } from '../services/productDetails.ts';
+import { getProducts } from '../services/productsService.ts';
+import cacheClient from '../cacheClient.ts';
 
 export const productDetailsController = async (
   req: Request,
@@ -8,7 +9,16 @@ export const productDetailsController = async (
 ) => {
   try {
     const productId = req.params.id;
-    const productDetails = await getProductDetails(productId);
+
+    const cacheKey = `productDetails-${productId}`;
+    const cachedData = cacheClient.get(cacheKey);
+
+    if (cachedData) {
+      return res.json(cachedData);
+    }
+
+    const productDetails = await getProducts({ id: productId });
+    cacheClient.set(cacheKey, productDetails);
 
     res.json(productDetails);
   } catch (error) {
--- bff/src/controllers/productsController.ts
+++ bff/src/controllers/productsController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { getProducts } from '../services/productsService.ts';
+import cacheClient from '../cacheClient.ts';
 
 export const productsController = async (
   req: Request,
@@ -8,7 +9,16 @@ export const productsController = async (
   next: NextFunction,
 ) => {
   try {
+    const cacheKey = `products-${Buffer.from(JSON.stringify(req.query), 'utf8').toString('base64')}`;
+    const cachedData = cacheClient.get(cacheKey);
+
+    if (cachedData) {
+      return res.json(cachedData);
+    }
+
     const products = await getProducts(req.query as Record<string, string>);
+    cacheClient.set(cacheKey, products);
+
     res.json(products);
   } catch (error) {
     next(error);
